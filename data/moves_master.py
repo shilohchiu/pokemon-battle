aef96cf888a9e4c3b6253a3b9206b2ca83114256
Shiloh <emily.chiu000@gmail.com>
2023-10-26 15:03:15 -0400

    added more files

---
+++ data/moves_master.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+moves = {
+    'hydro_pump':{'type':'water','base_damage':'120','side_effect':'none'},
+    'dark_pulse':{'type':'water','base_damage':'120','side_effect':'flinch'}
+}
+
+# access type and figure out multipliers
